94111faa1dfb9572d22fdc6ae5f6792fa1567ec6
TitasUnique <saha.titas.2016@gmail.com>
2024-02-21 08:45:00 +0530

    Navbar changes for arrow

--- src/components/Highlights/Highlights.jsx
+++ src/components/Highlights/Highlights.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './Highlights.css'
 const Highlights = () => {
   return (
-    <div className='highlight'><h1>Highlights</h1></div>
+    <div>
+      <div className='highlight'><h1>Highlights</h1></div>
+      <div>
+        <i className="fa-solid fa-arrow-right"></i>
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.css'
-const navbar = () => {
+const Navbar = () => {
   return (
     <div className="n-wrapper">
       <div className="n-left">
@@ -8,62 +8,73 @@ const navbar = () => {
       </div>
 
       <div className="n-right">
-          <ul className='n-right-list-ul'>
-            <li className="Edu-list">
-              <a href="https://react.dev/">Education <i className="fa-solid fa-caret-down" /></a>
-              <div className="sub-menu1">
-                <ul>
-                  <li><a href="https://react.dev/">10th Standard</a></li>
-                  <li><a href="https://react.dev/">12th Standard</a></li>
-                  <li><a href="https://react.dev/">Graduation</a></li>
-                </ul>
-              </div>
-            </li>
+        <ul className='n-right-list-ul'>
+          <li className="Edu-list">
+            <a href="https://react.dev/">Education
+              <i className="fa-solid fa-arrow-right"></i>
+            </a>
+            <div className="sub-menu1">
+              <ul>
+                <li><a href="https://react.dev/">10th Standard</a></li>
+                <li><a href="https://react.dev/">12th Standard</a></li>
+                <li><a href="https://react.dev/">Graduation</a></li>
+              </ul>
+            </div>
+          </li>
 
-            <li className='Exp-list'>
-              <a href="https://react.dev/">Experiences</a>
-              <div className="sub-menu2">
-                <ul className='sub-menu2-ul'>
-                  <li><a href="https://react.dev/">LearnElectronics India</a></li>
-                  <li><a href="https://react.dev/">Wipro</a></li>
-                </ul>
-              </div>
-            </li>
+          <li className='Exp-list'>
+            <a href="https://react.dev/">Experiences
+              <i className="fa-solid fa-arrow-right"></i>
+            </a>
+            <div className="sub-menu2">
+              <ul className='sub-menu2-ul'>
+                <li><a href="https://react.dev/">LearnElectronics India</a></li>
+                <li><a href="https://react.dev/">Wipro</a></li>
+              </ul>
+            </div>
+          </li>
 
-            <li className='Proj-list'>
-              <a href="https://react.dev/">Projects</a>
-              <div className="sub-menu3">
-                <ul className='sub-menu3-ul'>
-                  <li><a href="https://react.dev/">Academic Projects</a></li>
-                  <li><a href="https://react.dev/">Work Projects</a></li>
-                  <li><a href="https://react.dev/">Personal Projects</a></li>
-                </ul>
-              </div>
-            </li>
+          <li className='Proj-list'>
+            <a href="https://react.dev/">Projects
+              <i className="fa-solid fa-arrow-right"></i>
+            </a>
+            <div className="sub-menu3">
+              <ul className='sub-menu3-ul'>
+                <li><a href="https://react.dev/">Academic Projects</a></li>
+                <li><a href="https://react.dev/">Work Projects</a></li>
+                <li><a href="https://react.dev/">Personal Projects</a></li>
+              </ul>
+            </div>
+          </li>
 
-            <li className='Work-list'>
-              <a href="https://react.dev/">WorkDemo</a>
-              <div className="sub-menu4">
-                <ul className='sub-menu4-ul'>
-                  <li><a href="https://react.dev/">Web Developement</a></li>
-                  <li><a href="https://react.dev/">Coding Analysis</a></li>
-                </ul>
-              </div>
-            </li>
+          <li className='Work-list'>
+            <a href="https://react.dev/">WorkDemo
+              <i className="fa-solid fa-arrow-right"></i>
+            </a>
+            <div className="sub-menu4">
+              <ul className='sub-menu4-ul'>
+                <li><a href="https://react.dev/">Web Developement</a></li>
+                <li><a href="https://react.dev/">Coding Analysis</a></li>
+              </ul>
+            </div>
+          </li>
 
-            <li className='Other-list'>
-              <a href="https://react.dev/">Others</a>
-              <div className="sub-menu5">
-                <ul className='sub-menu5-ul'>
-                  <li><a href="https://react.dev/">Hobby</a></li>
-                </ul>
-              </div>
-            </li>
-          </ul>
+          <li className='Other-list'>
+            <a href="https://react.dev/">Others
+            <i className="fa-solid fa-arrow-right"></i></a>
+            <div className="sub-menu5">
+              <ul className='sub-menu5-ul'>
+                <li>
+                  <a href="https://react.dev/">Hobby</a>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
         <button className='button'>Contact</button>
       </div>
     </div>
   )
 }
 
-export default navbar
+export default Navbar
