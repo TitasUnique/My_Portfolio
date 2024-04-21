24c78577716d3f492e07336fa6160fa18c9c1b72
TitasUnique <saha.titas.2016@gmail.com>
2024-04-21 19:35:43 +0530

    school and college page responsive

--- src/components/College/College.jsx
+++ src/components/College/College.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './College.css'
 import 'animate.css'
 import CollegeDays1 from '../../Images/College_images/CollegeDays01.jpg'
@@ -16,26 +16,30 @@ const College = () => {
 
   const [currentImageIndex, setcurrentImageIndex] = useState(0);
 
-  const images = [Ilovejis1,CollegeDays1,CollegeDays2,Hatsoff,Ilovejis2,FirstDay,Industry,Lastday,jistech];
+  const images = [Ilovejis1, CollegeDays1, CollegeDays2, Hatsoff, Ilovejis2, FirstDay, Industry, Lastday, jistech];
 
   useEffect(() => {
-    const interval = setInterval(() => {setcurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);}, 2000);
+    const interval = setInterval(() => { setcurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length); }, 2000);
     return () => clearInterval(interval);
   }, [images.length]);
 
   return (
     <>
-    <Navbar navold={"white"}/>
-    <div className='college'>
-      <div className="collegeName animate__animated animate__zoomInDown">JIS COLLEGE OF ENGINEERING, KALYANI</div>
-      <div className="college-descrip-box">
+      <div className='college'>
+        <div className="college-back-img"></div>
+        <Navbar navold={"white"} />
+        <div className="content-college">
+          <div className="collegeName animate__animated animate__zoomInDown">JIS COLLEGE OF ENGINEERING, KALYANI</div>
+          <div className="college-down">
+            <div className="college-descrip-box">Hello hi!</div>
+            <div className="college-img-scroll">
+              {images.map((image, index) => (
+                <img key={index} src={image} alt="" className={index === currentImageIndex ? 'active' : ''} />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="college-img-scroll">
-        {images.map((image, index) => (
-          <img key={index} src={image} alt="" className={index === currentImageIndex ? 'active' : ''} />
-        ))}
-      </div>
-    </div>
     </>
   )
 }
--- src/components/School/School.jsx
+++ src/components/School/School.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './School.css'
 import 'animate.css'
 import oldgroup from '../../Images/School_images/old-group.jpg'
@@ -10,26 +10,30 @@ const School = () => {
 
   const [currentImageIndex, setcurrentImageIndex] = useState(0);
 
-  const images = [oldgroup,oldschool,rpchsschool,rpchsstudent];
+  const images = [oldgroup, oldschool, rpchsschool, rpchsstudent];
 
   useEffect(() => {
-    const interval = setInterval(() => {setcurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);}, 2000);
+    const interval = setInterval(() => { setcurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length); }, 2000);
     return () => clearInterval(interval);
   }, [images.length]);
 
   return (
     <>
-    <Navbar navold={"white"}/>
-    <div className='school'>
-      <div className="name animate__animated animate__zoomInDown">PAL CHOWDHURY HIGH SCHOOL</div>
-      <div className="descrip-box">
+      <div className='school'>
+        <div className="back-image"></div>
+        <Navbar navold={"white"} />
+        <div className="content-school">
+          <div className="name-up animate__animated animate__zoomInDown">PAL CHOWDHURY HIGH SCHOOL</div>
+          <div className="down">
+            <div className="descrip-box">Hello Hi</div>
+            <div className="img-scroll">
+              {images.map((image, index) => (
+                <img key={index} src={image} alt="" className={index === currentImageIndex ? 'active' : ''} />
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="img-scroll">
-        {images.map((image, index) => (
-          <img key={index} src={image} alt="" className={index === currentImageIndex ? 'active' : ''} />
-        ))}
-      </div>
-    </div>
     </>
   )
 }
